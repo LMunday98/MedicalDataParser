05c23dbbd43a1e650b911fd9f7644c48cf0f872a
LMunday98 <luke.munday@gmail.com>
2021-01-22 21:16:11 +0000

    Parses headers to array list

--- FileHandler.java
+++ FileHandler.java
@@ -2,6 +2,8 @@ import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
 
+import java.util.ArrayList;
+
 public class FileHandler {
 
   private String start_col;
@@ -11,24 +13,27 @@ public class FileHandler {
   private String line;
   private String splitBy;
 
-  private boolean col_headers;
+  private boolean got_headers;
+  private ArrayList<String> col_headers;
 
   public FileHandler(String _start_col, String _sort_col, String _file_name) {
     this.start_col = _start_col;
     this.sort_col = _sort_col;
-    this.file_path = _file_name;
+    this.file_path = "data/" + _file_name;
 
     this.line = "";
     this.splitBy = ",";
 
-    this.col_headers = false;
+    this.got_headers = false;
+    this.col_headers = new ArrayList<String>();
    }
 
    public void ParseFile() {
-     try   {
+     try {
       BufferedReader br = new BufferedReader(new FileReader(file_path));
       while ((line = br.readLine()) != null) {
-      String[] row = line.split(splitBy);    // use comma as separator
+      String[] row = line.split(splitBy);
+        ParseHeaders(row);
         ParsePatient(row);
       }
     }
@@ -37,9 +42,24 @@ public class FileHandler {
     }
    }
 
-   private void ParsePatient(String[] patient_row) {
+   private void ParseHeaders(String[] row) {
+     if (!got_headers) {
+       for (String data : row) {
+         col_headers.add(data);
+       }
+       got_headers = true;
+     }
+   }
+
+   private void ParsePatient(String[] row) {
      for (String data : row) {
 
+     }
+   }
+
+   public void EchoData() {
+     for (String data : col_headers) {
+       System.out.println(data);
      }
    }
 }
--- Main.java
+++ Main.java
@@ -2,5 +2,6 @@ public class Main {
  public static void main(String[] args) {
    FileHandler fh = new FileHandler("Encounterid", "Photograph_Date", "data.csv");
    fh.ParseFile();
+   fh.EchoData();
  }
 }
